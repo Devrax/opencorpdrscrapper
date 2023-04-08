fe7019a5d34f6640642a4808d3bcd455259b5aed
Rafael Mejia <devrafx@gmail.com>
2023-04-08 17:37:01 -0400

    chore: Add process.env.PORT
    chore: Change script start to be dev

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import express from "express";
 import { companies, company } from "./routes/index.js";
 
 const app = express();
-
-app.get("/", function(req, res) {
-  res.send("Hey");
-});
 app.get("/companies", companies);
 app.get("/company", company);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on http://localhost:3000/");
 });
